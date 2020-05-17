7a6ef333348f7dd13f2c3d4b814ced661f3cafdc
Jesse Greathouse <jesse.greathouse@gmail.com>
2020-05-17 13:23:29 -0600

    switched column order of upstream_server

--- src/jesse-greathouse/loba/lib/sql/mysql/create_table_upstream_server.sql
+++ src/jesse-greathouse/loba/lib/sql/mysql/create_table_upstream_server.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `upstream_server` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `host` VARCHAR(255) NULL,
-  `weight` INT NULL,
   `backup` TINYINT NOT NULL DEFAULT 0,
+  `upstream_id` INT NOT NULL,
+  `weight` TINYINT NULL,
   `fail_timeout` TINYINT NULL,
   `max_fails` TINYINT NULL,
-  `upstream_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_upstream_id_idx` (`upstream_id` ASC),
   INDEX `weight` (`weight` DESC),
