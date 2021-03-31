6aff74aaf94d0677afdc6143cedf64c03d8c1d07
jesse-greathouse <jesse.greathouse@gmail.com>
2021-03-31 17:08:07 -0600

    fixed problem where nginx upstream templates werent rendering correctly when the site does not have security certificates and keys

--- src/jesse-greathouse/loba/lib/sql/mysql/upstream_join_by_site_id.sql
+++ src/jesse-greathouse/loba/lib/sql/mysql/upstream_join_by_site_id.sql
@@ -2,5 +2,5 @@ SELECT s.`domain`, m.`directive`, u.`hash`, u.`consistent`, u.`ssl`, c.`certific
 FROM `upstream` u
 INNER JOIN `site` s ON  s.`id` =  u.`site_id`
 INNER JOIN `method` m ON m.`id` = u.`method_id`
-INNER JOIN `upstream_certificate` c ON c.`upstream_id` =  u.`id`
+LEFT JOIN `upstream_certificate` c ON c.`upstream_id` =  u.`id`
 WHERE u.`site_id` = ?;
--- src/jesse-greathouse/loba/lib/views/api/upstream.lua
+++ src/jesse-greathouse/loba/lib/views/api/upstream.lua
@@ -1,8 +1,24 @@
 local base = require "views.api.base"
+local cjson = require "cjson"
 local setmetatable = setmetatable
 
 local _M = {}
 local mt = { __index = _M }
+
+function _M:get_post()
+    local args, err = base.get_post(self)
+    if err then
+        ngx.log(ngx.ERR, "bad post args: ", cjson.encode(args), " ", err)
+        return ngx.exit(500)
+    end
+
+    -- workaround for db:execute problem
+    if args.hash == ngx.null then
+        args.hash = ""
+    end
+
+    return args, err
+end
 
 function _M.new(self, route)
     return setmetatable(base:new('upstream', route), mt)
