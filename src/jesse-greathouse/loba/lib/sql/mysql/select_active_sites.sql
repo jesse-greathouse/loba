ac0b765a3e03af28712b6d95a0f9fd25385c1275
jesse-greathouse <jesse.greathouse@gmail.com>
2021-03-31 17:52:39 -0600

    fix that will prevent rendering nginx upstream configs when the upstream has no servers

--- src/jesse-greathouse/loba/lib/sql/mysql/select_active_sites.sql
+++ src/jesse-greathouse/loba/lib/sql/mysql/select_active_sites.sql
@@ -1,1 +1,7 @@
-SELECT * FROM `site` WHERE `active` = 1;
+SELECT *
+FROM `site` s
+INNER JOIN `upstream` u ON u.`site_id` = s.`id`
+INNER JOIN (
+	SELECT DISTINCT `upstream_id` FROM `upstream_server`
+) us ON us.`upstream_id` = u.`id`
+WHERE s.`active` = 1;
