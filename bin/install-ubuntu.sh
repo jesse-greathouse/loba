2a2f13cd51472c18c6fa1716e0ae92299a3018aa
jesse-greathouse <jesse.greathouse@gmail.com>
2021-04-20 14:11:11 -0600

    corrected nvm install in the script

--- bin/install-ubuntu.sh
+++ bin/install-ubuntu.sh
@@ -92,7 +92,9 @@ cd ${DIR}
 
 # Install nvm and angular
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-exec bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 14.4
 nvm use 14.4
 npm install -g @angular/cli
