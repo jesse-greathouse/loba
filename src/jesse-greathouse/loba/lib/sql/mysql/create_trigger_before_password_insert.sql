3d9cfc8d9335e688f6aab16c760442fb007e5a08
jesse-greathouse <jesse.greathouse@gmail.com>
2021-04-21 12:08:31 -0600

    updated npm lock file and corrected syntax in trigger creation sql

--- src/jesse-greathouse/loba/lib/sql/mysql/create_trigger_before_password_insert.sql
+++ src/jesse-greathouse/loba/lib/sql/mysql/create_trigger_before_password_insert.sql
@@ -1,7 +1,8 @@
 DROP TRIGGER IF EXISTS `password_BEFORE_INSERT`;
 
-CREATE TRIGGER `password_BEFORE_INSERT` BEFORE INSERT ON `password` FOR EACH ROW
-BEGIN
+DELIMITER $$
+CREATE DEFINER=CURRENT_USER TRIGGER `password_BEFORE_INSERT` BEFORE INSERT ON `password` FOR EACH ROW BEGIN
   SET new.`salt` = sha2(rand(), 224);
   SET new.`password` = sha2(concat(new.`password`, new.`salt`), 224);
-END
+END$$
+DELIMITER ;
