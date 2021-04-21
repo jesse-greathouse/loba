cba6f6255ab23112bf4b86dc242f513f0e5bf7da
jesse-greathouse <jesse.greathouse@gmail.com>
2021-04-21 12:28:48 -0600

    changed delimiter syntax in trigger create

--- src/jesse-greathouse/loba/lib/sql/mysql/create_trigger_before_password_insert.sql
+++ src/jesse-greathouse/loba/lib/sql/mysql/create_trigger_before_password_insert.sql
@@ -1,8 +1,9 @@
 DROP TRIGGER IF EXISTS `password_BEFORE_INSERT`;
 
-DELIMITER __
+DELIMITER //
 CREATE DEFINER=CURRENT_USER TRIGGER `password_BEFORE_INSERT` BEFORE INSERT ON `password` FOR EACH ROW BEGIN
   SET new.`salt` = sha2(rand(), 224);
   SET new.`password` = sha2(concat(new.`password`, new.`salt`), 224);
-END__
+END;
+//
 DELIMITER ;
